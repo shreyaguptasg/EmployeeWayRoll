9ffac7df0c0475a2231ccbf2e70030712494dc02
shrey <shreya272002gupta@gmail.com>
2025-03-06 12:14:57 +0530

    [Shreya] Add. Store daily wage in uc6

--- ChcekEmployeePresence.js
+++ ChcekEmployeePresence.js
@@ -58,3 +58,12 @@ const calculateWagesTillCondition = () => {
     return { totalWage, totalHours, daysWorked };
   };
   console.log(calculateWagesTillCondition());
+
+  //uc6
+let dailyWages = [];
+const storeDailyWages = () => {
+  for (let i = 0; i < 20; i++) {
+    dailyWages.push(getDailyWage());
+  }
+};
+console.log(storeDailyWages());
